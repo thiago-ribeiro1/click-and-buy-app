73eaad1b7df22b99c01eae920d27b2a0ec830fef
Thiago Ribeiro <thiagoribeiroramos_@outlook.com>
2025-03-29 02:15:56 -0300

    Ajustes animações SplashScreen

--- ecommerce-app/src/pages/SplashScreen/SplashScreen.tsx
+++ ecommerce-app/src/pages/SplashScreen/SplashScreen.tsx
@@ -6,16 +6,18 @@ import { StyleSheet, View, Image, StatusBar } from "react-native";
 const SplashScreen: React.FC = () => {
   return (
     <View style={styles.container}>
-        <StatusBar backgroundColor="#0B8FAC" barStyle="light-content" />
+        <StatusBar backgroundColor="#F0F8FF" barStyle="light-content" />
         <LottieView
         source={require("../../../assets/animations/Animation - 1743218939341.json")}
         autoPlay
         loop
         style={styles.animation}
       />
-      <Image
-        style={styles.logo}
-        source={require("../../../assets/img/click&buy-white.png")}  
+      <LottieView
+        source={require("../../../assets/animations/Animation - 1743225079384.json")}
+        autoPlay
+        loop
+        style={styles.secondaryAnimation}
       />
     </View>
   );
@@ -26,15 +28,15 @@ export default SplashScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1, // Ocupa toda a tela
-    backgroundColor: "#0B8FAC", // Cor do fundo
+    backgroundColor: "#F0F8FF", // Cor do fundo
     justifyContent: "center", // Centraliza verticalmente
     alignItems: "center", // Centraliza horizontalmente
   },
-  logo: {
-    width: 240, // tamanho da logo
+  secondaryAnimation: {
+    width: 240,
     height: 120,
-    resizeMode: "contain", // Garante que a imagem não distorça
-    marginTop: 10, // Espaço entre a logo e a animação 
+    resizeMode: "contain",
+    marginTop: 10,
   },
   animation: {
     width: 380,    
