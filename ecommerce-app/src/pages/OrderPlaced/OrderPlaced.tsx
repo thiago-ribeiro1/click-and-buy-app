7c8256a402500bcd0ce325e2a1ea99d89c511900
Thiago Ribeiro <thiagoribeiroramos_@outlook.com>
2025-04-03 00:50:28 -0300

    Animações tela de Pedido Concluído

--- ecommerce-app/src/pages/OrderPlaced/OrderPlaced.tsx
+++ ecommerce-app/src/pages/OrderPlaced/OrderPlaced.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
-import { Image, Text, View, StatusBar } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../NavigationTypes/navigationTypes";
 import styles from "./OrderPlacedStyle";
 import { useCart } from "../Cart/CartContext";
+import LottieView from "lottie-react-native";
 
 const OrderPlaced: React.FC = () => {
-
   // Define a navegação para esta tela
   type OrderPlacedScreenNavigationProp = StackNavigationProp<RootStackParamList, "OrderPlaced">;
   const navigation = useNavigation<OrderPlacedScreenNavigationProp>();
@@ -15,7 +15,7 @@ const OrderPlaced: React.FC = () => {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-    clearCart();
+      clearCart();
       navigation.replace("HomeScreen"); // Substitui a tela atual pela HomeScreen em 4 segundos
     }, 4000);
 
@@ -24,20 +24,26 @@ const OrderPlaced: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor="#c6f7f1" barStyle="light-content" />
-      {/* Fundo Azul + Imagem */}
-      <Image
-        style={styles.backgroundImage}
-        source={require("../../../assets/img/order-placed.png")}
+      <StatusBar backgroundColor="aliceblue" barStyle="light-content" />
+      
+      {/* Animação de cima */}
+      <LottieView
+        source={require("../../../assets/animations/Animation - 1743648942565.json")}
+        autoPlay
+        loop
+        style={styles.secondaryAnimation}
       />
 
       {/* Conteúdo centralizado */}
       <View style={styles.content}>
         <Text style={styles.title}>Pedido Concluído</Text>
 
-        <Image
-          style={styles.checkIcon}
-          source={require("../../../assets/img/concluido.png")}
+        {/* Animação Maquininha */}
+        <LottieView
+          source={require("../../../assets/animations/Animation - 1743649215050.json")}
+          autoPlay
+          loop
+          style={styles.animation}
         />
       </View>
     </View>
--- ecommerce-app/src/pages/OrderPlaced/OrderPlacedStyle.tsx
+++ ecommerce-app/src/pages/OrderPlaced/OrderPlacedStyle.tsx
@@ -1,42 +1,48 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FFFFFF",
-        justifyContent: "flex-start",
-        alignItems: "center",
-      },
-      backgroundImage: {
-        position: "absolute", 
-        top: 0, 
-        left: 0, 
-        width: "100%",
-        height: "60%",  
-        resizeMode: "cover",
-      },
-      content: {
-        width: "100%",
-        height: "40%",
-        backgroundColor: "#F4F4F4",
-        alignItems: "center",
-        justifyContent: "center",
-        paddingTop: 40,
-        position: "absolute",
-        bottom: 0,
-        borderTopLeftRadius: 32,
-        borderTopRightRadius: 32,
-      },
-      title: {
-        fontSize: 32,
-        fontWeight: "700",
-        color: "#272727",
-        textAlign: "center",
-        marginBottom: 20,
-      },
-      checkIcon: {
-        width: 125,
-        height: 125,
-        marginBottom: 20,
-      },
-    });
+  container: {
+    flex: 1,
+    backgroundColor: "aliceblue",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingTop: 40,
+  },
+  backgroundImage: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "60%",
+    resizeMode: "cover",
+  },
+  content: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "#272727",
+    textAlign: "center",
+  },
+  checkIcon: {
+    width: 125,
+    height: 125,
+    marginBottom: 20,
+  },
+  secondaryAnimation: {
+    width: 380,
+    height: 300,
+    resizeMode: "contain",
+    marginTop: 15, // espaço entre a animação de cima e o título
+  },
+  animation: {
+    width: 380,
+    height: 340,
+    resizeMode: "contain",
+    marginBottom: 45,
+  },
+});
+ 
