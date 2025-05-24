958dcc79aa5b582b1da32d48948130e99f5aeb25
Thiago Ribeiro <thiagoribeiroramos_@outlook.com>
2025-05-23 22:11:06 -0300

    feat: correções valor subtotal

--- ecommerce-app-backend/public/src/models/Order.js
+++ ecommerce-app-backend/public/src/models/Order.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    products: [{ type: Schema.Types.ObjectId, ref: 'Product', required: true }],
-    total: { type: Number, required: true },
-    orderDate: { type: Date, default: Date.now }
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  items: [
+    {
+      product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+      quantity: { type: Number, required: true },
+      unitPrice: { type: Number, required: true },
+      subtotal: { type: Number, required: true },
+    },
+  ],
+  total: { type: Number, required: true },
+  orderDate: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Order', OrderSchema);
+module.exports = mongoose.model("Order", OrderSchema);
