e8b5bda35f5d952e1b29973efc31a30eebb01abd
Thiago Ribeiro <thiagoribeiroramos_@outlook.com>
2026-02-10 23:48:55 -0300

    atualiza versão do modelo Gemini

--- ecommerce-app-backend/services/IaService.js
+++ ecommerce-app-backend/services/IaService.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.5-flash",
 });
 
 const aiService = {
